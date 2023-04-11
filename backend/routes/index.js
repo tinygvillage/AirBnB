bb09b75f90a7d1b7d4b9f83a593126741bc10de1
tinygvillage <connect@refusionist.com>
2023-04-11 14:34:47 -0700

    Add error-handling middleware for common errors and debugging

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+
 //XSRF-TOKEN
 // router.get('/hello/world', function (req, res) {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -15,5 +18,7 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
